69d058ecaf534080967d3788336788865abb4c7e
Aman J Upadhyay <24upadhyaya@rbunagpur.in>
2026-03-12 12:00:08 +0530

    Create pythonclass2trainningbasic.py

---
+++ pythonclass2trainningbasic.py
@@ -0,0 +1,110 @@
+# mydict={
+#     "name":"prashant",
+#     "professional":"developer",
+#     "empid":1001
+# }
+
+# print(type(mydict))
+
+# mydict={
+#     101:"prasant",
+#     102:"ashish",
+#     "103":"mohini",
+#     "104":"trivani",
+#     101:"ashish",
+#     104:"ashish"
+# }
+# # print(mydict)
+# # a=mydict[102]
+# # print(a)
+
+# mydict[102]="peter"
+# print(mydict)
+
+# for x in mydict:
+#     print(x)
+
+# for x,y in mydict.items():
+#     print(x,y)
+
+# mydict["mobile_no"]=4646463738
+# print(mydict)
+
+# mydict.pop(101)
+# print(mydict)
+
+#string slicing
+
+# name="prasahant"
+
+# print(name[0])
+# print(name[1])
+# print(name[-1])
+# print(name[0:5])
+# print(name[1:])
+# print(name[:5])
+# print(name[:])
+# print(name[1:8:2])
+# print(name[::-1])
+
+# s="h i b l f r p"
+# print(s.find("h"))
+# print(s.find("r"))
+# print(s.find("b"))
+
+# s="aman","ashish","sumit"
+# m="|".join(s)
+# print(m)
+
+# s="i am great full"
+# print(s.lower())
+# print(s.upper())
+# print(s.swapcase())
+# print(s.title())
+# print(s.capitalize())
+
+# print('subject marks:')
+# phy=50
+# chem=60
+# math=70
+# print("physics={} chemistry={} math={}" .format(phy,chem,math))
+# print("physics={0} chemistry={1} math={2}" .format(phy,chem,math))
+# totle=phy+chem+math
+# print("totle marks",f"{totle}")
+# print("roll no=","7".zfill(4))
+
+# print('prashantjha777'.isalnum())
+# print('prashantjha'.isalpha())
+# print('777f'.isdigit())
+# print('sdsdsdsd'.islower())
+# print('PRASANTj'.isupper())
+# print(''.istitle())
+# print(''.isspace())
+# print("hello".startswith("he"))
+# print("hello".endswith("lo"))
+
+# a=50
+# b=30
+# c=20
+# d=10
+# print((a+b)*c/d)
+
+# x=['A','B','C']
+# y=['A','B','C']
+# z=[1,2,3,4]
+# print(x==y)
+# print(x==z)
+# print(x !=z)
+
+name="prashant"
+# count=0
+# count1=0
+# for i in name:
+#     print(i," ",end="")
+#     if(i=="a" or i=="e" or i=="i" or i=="0" or i=="u"):
+#         count=count+1
+#     else:
+#         count1=count1+1
+# print()    
+# print(count)      
+# print(count1)  
